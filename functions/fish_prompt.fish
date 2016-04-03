eff0adcea012feca394e081204b1d73b6b6c8fe4
Sota Yamashita <sota.yamashita@gmail.com>
2016-04-03 17:33:25 +0900

    Add a few function

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,10 +1,44 @@
 # Simple
 # https://github.com/sotayamashita/simple
 #
-# Color from http://www.colorhunt.co/c/16765
-# Prompt from https://github.com/mathiasbynens/dotfiles/blob/master/.bash_prompt
-#
 # MIT © Sota Yamashita
+
+function git_upstream_configured
+  git rev-parse --abbrev-ref @"{u}" > /dev/null 2>&1
+end
+
+function git_behind_upstream
+  test (git rev-list --right-only --count HEAD...@"{u}" ^ /dev/null) -gt 0
+end
+
+function git_ahead__upstream
+  test (git rev-list --left-only --count HEAD...@"{u}" ^ /dev/null) -gt 0
+end
+
+function git_upstream_status
+  set -l upsream_status
+
+  if git_upstream_configured
+    if git_behind_upstream
+      set upsream_status "$upsream_status⇣"
+    end
+
+    if git_ahead__upstream
+      set upsream_status "$upsream_status⇡"
+    end
+  end
+
+  echo $upsream_status
+end
+
+function print_color
+  set -l color  $argv[1]
+  set -l string $argv[2]
+
+  set_color $color
+  printf $string
+  set_color normal
+end
 
 function fish_prompt -d "Simple Fish Prompt"
   echo -e ""
@@ -28,10 +62,10 @@ function fish_prompt -d "Simple Fish Prompt"
   #
   set -l pwd_glyph " in "
   set -l pwd_color -o 5DAE8B
-  set -l pwd_path $PWD
+  set -l pwd_string (echo $PWD | sed 's|^'$HOME'\(.*\)$|~\1|')
 
   echo_color fff "$pwd_glyph"
-  echo_color $pwd_color "~$pwd_path" | sed -E "~s|$HOME||;s||g;s|g"
+  echo_color $pwd_color "$pwd_string"
 
 
   # Git
@@ -46,19 +80,10 @@ function fish_prompt -d "Simple Fish Prompt"
     echo_color $git_branch_color "$branch_name"
 
     # check if there is an upstream
-    set -l git_ahead (command git rev-list --left-right --count "HEAD...origin/master" ^ /dev/null | awk '
-        $1 > 0 { printf("⇡") } # can push
-        $2 > 0 { printf("⇣") } # can pull
-    ')
-
-    if test ! -z "$git_ahead"
-        echo_color fff "$git_ahead"
-    end
+    print_color 5DAE8B " $git_upstream_status"
   end
 
   # Prompt
   #
-  echo -e ""
-  echo_color FF7676 "❯ "
-
+  print_color FF7676 "\n❯ "
 end
