4be4d3344d534fc150afff4aa1a3fff45a4ed932
Jorge Bucaran <j@bucaran.me>
2016-04-18 14:32:29 +0900

    Remove echo_color dependency.

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -10,11 +10,11 @@ function fish_right_prompt
         set -l duration_copy $CMD_DURATION
         set -l duration (echo $CMD_DURATION | humanize_duration)
 
-        echo_color $status_color "$duration"
+         echo -sn (set_color $status_color) "$duration" (set_color normal)
 
     else if set -l last_job_id (last_job_id)
-        echo_color $status_color "%$last_job_id"
+         echo -sn (set_color $status_color) "%$last_job_id" (set_color normal)
     else
-        echo_color 555 (date "+%H:%M")
+         echo -sn (set_color 555) (date "+%H:%M") (set_color normal)
     end
 end
